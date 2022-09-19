9d5dcec06486c44edef93383dacd6f53b5a2ab00
letconstvar <1985225667@qq.com>
2022-09-19 15:04:55 +0800

    chore: init vue-ts-template

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+import eslintPlugin from 'vite-plugin-eslint'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue']
+    })
+  ],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve('src') }
+    ]
+  }
+})
