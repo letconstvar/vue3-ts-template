96498bb331f6b4086eaa63502e1b33c8b7726937
letconstvar <1985225667@qq.com>
2022-10-10 16:39:50 +0800

    chore: 设置 src 文件夹别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
-import eslintPlugin from 'vite-plugin-eslint'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslintPlugin({
-      include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue']
-    })
+      include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+    }),
   ],
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve('src') }
-    ]
-  }
-})
+    alias: [{ find: '@', replacement: path.resolve('src') }],
+  },
+  server: {
+    proxy: {
+      '^/proxy/.*': {
+        target: 'http://192.168.31.185:60603',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy/, ''),
+      },
+    },
+  },
+});
