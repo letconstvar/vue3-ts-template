a55a59bde5d3f6ec844d08bff5accb2b74ff89ed
letconstvar <1985225667@qq.com>
2022-10-10 16:42:27 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import { postLoginApi } from '@/api/login';
+
+const test = async () => {
+  const results = await postLoginApi({
+    username: 'admin',
+    password: 'admin',
+    rememberMe: true,
+  });
+  console.log(results);
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="test()">http test</button>
 </template>
 
 <style scoped>
