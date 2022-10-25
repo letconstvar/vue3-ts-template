4831aff2d446a5cb4db6aab654a6fea9daf3b6a6
letconstvar <1985225667@qq.com>
2022-10-25 17:07:35 +0800

    style: 支持jsx, tsx语法

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  parser: "vue-eslint-parser",
-  extends: ["plugin:vue/essential", "plugin:prettier/recommended"],
+  parser: 'vue-eslint-parser',
+  extends: ['plugin:vue/essential', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaFeatures: {
+      // Allows for the parsing of JSX
+      jsx: true,
+    },
   },
-  plugins: ["vue", "@typescript-eslint"],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    "block-spacing": [2, "always"],
-    "brace-style": [
+    'block-spacing': [2, 'always'],
+    'brace-style': [
       2,
-      "1tbs",
+      '1tbs',
       {
         allowSingleLine: true,
       },
@@ -23,11 +27,11 @@ module.exports = {
     // 有分号结尾
     semi: [
       2,
-      "always",
+      'always',
       {
         omitLastInOneLineBlock: true,
       },
     ],
-    "vue/no-multiple-template-root": 0,
+    'vue/no-multiple-template-root': 0,
   },
 };
