64bb1e367ce558af33ed19992099c2e48983e914
letconstvar <1985225667@qq.com>
2022-11-09 18:20:01 +0800

    ref: encapsulate api request

--- src/api/login/index.ts
+++ src/api/login/index.ts
@@ -2,10 +2,10 @@ import http from '@/utils/http';
 import { PostLoginParams } from './types';
 
 enum API {
-  Login = '/api/v1/login',
+  Login = '/test',
 }
 
 export const postLoginApi = async (data: PostLoginParams) => {
   const { data: results } = await http.post<string>(API.Login, data);
-  return results;
+  return results.errorCode === 10200 ? results.data : null;
 };
